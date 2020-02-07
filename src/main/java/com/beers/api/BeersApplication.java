fefab5afb1486de9162f0dc683d65e08445945da
Gabriel Babler <gabrielbabler@yahoo.com.br>
2020-02-07 14:23:59 -0300

    base project

---
+++ src/main/java/com/beers/api/BeersApplication.java
@@ -0,0 +1,13 @@
+package com.beers.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beers/api/BeersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beers.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
